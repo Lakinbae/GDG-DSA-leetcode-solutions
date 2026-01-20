e191545495e3aded077d1ae8f5e8934354d06a7f
Lakinbae <lakinawel42@gmail.com>
2026-01-20 14:45:56 +0300

    added n

--- P2413.py
+++ P2413.py
@@ -1,4 +1,8 @@
 class Solution:
     def smallestEvenMultiple(self, n: int) -> int:
         return n if n%2==0 else n*2
-    print(n)
+    if n == "main":
+        test_value= 5
+        result= Solution().smallestEvenMultiple(test_value)
+        print(f"The smallest even multiple of {test_value} is:{result}")
+    
