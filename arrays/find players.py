81f5db983c6fbbbc3ca76834689fa8fdbd7b2712
Lakin <lakin.awel@aastustudent.edu.et>
2026-04-25 07:36:42 +0300

    Update find players.py

--- arrays/find players.py
+++ arrays/find players.py
@@ -1,1 +1,13 @@
-kk
+class Solution:
+    def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
+        losses = {}
+        players = set()
+        for winner, loser in matches:
+            players.add(winner)
+            players.add(loser)
+            losses[loser] = losses.get(loser, 0) + 1
+            
+        zero_loss = sorted([p for p in players if p not in losses])
+        one_loss = sorted([p for p in players if losses.get(p) == 1])
+        
+        return [zero_loss, one_loss]
